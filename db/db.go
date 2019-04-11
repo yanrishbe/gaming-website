743806824cdc58ea702e86b82d6ab7ba83f8d42d
Yana Strabuk <yana.strabuk@gmail.com>
2019-04-11 19:52:52 +0300

    Refactor db functions

--- db/db.go
+++ db/db.go
@@ -11,7 +11,7 @@ import (
 // DB struct stores users' data in UsersMap
 type DB struct {
 	mutex        *sync.RWMutex
-	UsersMap     map[int]*entities.User
+	UsersMap     map[int]entities.User
 	UsersCounter int
 }
 
@@ -23,32 +23,35 @@ func canRegister(user entities.User) bool {
 }
 
 // SaveUser registers a new user
-func (db *DB) SaveUser(user *entities.User) (int, error) {
-	if !canRegister(*user) {
-		return 0, errors.New("user's data is not valid")
+func (db *DB) SaveUser(usr entities.User) (entities.User, error) {
+	us := usr
+	if !canRegister(us) {
+		return us, errors.New("user's data is not valid")
 	}
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
 	db.UsersCounter++
-	user.Balance -= 300
-	db.UsersMap[db.UsersCounter] = user
-	return db.UsersCounter, nil
+	us.ID = db.UsersCounter
+	us.Balance -= 300
+	db.UsersMap[db.UsersCounter] = us
+	return us, nil
 }
 
-func (db *DB) GetUser(id int) (*entities.User, error) {
+func (db *DB) GetUser(id int) (entities.User, error) {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
-	user, doesExist := db.UsersMap[id]
+	us, doesExist := db.UsersMap[id]
 	if !doesExist {
-		return nil, errors.New("the id cannot match any user")
+		return us, errors.New("the id cannot match any user")
 	}
-	return user, nil
+	return us, nil
 }
 
 // DeleteUser removes a user from the UsersMap
 func (db *DB) DeleteUser(id int) error {
-	if _, errGet := db.GetUser(id); errGet != nil {
-		return errGet
+	_, err := db.GetUser(id)
+	if err != nil {
+		return err
 	}
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
@@ -57,38 +60,43 @@ func (db *DB) DeleteUser(id int) error {
 }
 
 // UserTake takes the requested amount of points from a user's balance
-func (db *DB) UserTake(id, points int) error {
-	if _, errGet := db.GetUser(id); errGet != nil {
-		return errGet
+func (db *DB) UserTake(id, points int) (entities.User, error) {
+	us, err := db.GetUser(id)
+	if err != nil {
+		return us, err
 	}
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
-	if db.UsersMap[id].Balance < points {
-		return errors.New("not enough balance to execute the request")
+	if us.Balance < points {
+		return us, errors.New("not enough balance to execute the request")
 	}
-	db.UsersMap[id].Balance -= points
-	return nil
+	us.Balance -= points
+	db.UsersMap[id] = us
+	return us, nil
 }
 
 // UserFund adds the requested amount of points to user's balance
-func (db *DB) UserFund(id, points int) error {
-	if _, errGet := db.GetUser(id); errGet != nil {
-		return errGet
+func (db *DB) UserFund(id, points int) (entities.User, error) {
+	us, err := db.GetUser(id)
+	if err != nil {
+		return us, err
 	}
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
-	db.UsersMap[id].Balance += points
-	return nil
+	us.Balance += points
+	db.UsersMap[id] = us
+	return us, nil
 }
 
 // New is used to create an instance of DB struct and initialize it
 func New() *DB {
 	return &DB{
 		mutex:    &sync.RWMutex{},
-		UsersMap: make(map[int]*entities.User),
+		UsersMap: make(map[int]entities.User),
 	}
 }
 
+// CountUsers returns the amount of elements in the db
 func (db *DB) CountUsers() int {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
